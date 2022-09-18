5438e189a95e81d8b64105c28cc4b24ec7a109f5
bestcode123 <callumfrederiksen97@gmail.com>
2022-09-18 16:02:13 +0100

    Weight Compute added

--- packages/src/pythonnn/models.py
+++ packages/src/pythonnn/models.py
@@ -22,7 +22,7 @@ class Sequential:
         else:
             raise Exception("Model is already compiled")
 
-    def compile(self, hard_drive=False):
+    def compile(self, hard_drive=False, preload=True):
         import numpy as np
 
         model = self.model
@@ -37,7 +37,10 @@ class Sequential:
                 for i in range(model[im][0]):
                     m2 = []
                     for j in range(model[im + 1][0]):
-                        m2.append(np.random.uniform(0, 1))
+                        m3 = []
+                        for k in range(model[im][0]):
+                            m3.append(np.random.uniform(0, 1))
+                        m2.append(m3)
                     m1.append(m2)
                 final.append(m1)
             modelout.append(final)
@@ -49,10 +52,37 @@ class Sequential:
                         m1.append(np.random.uniform(0, 1))
                     final.append([m1])
             modelout.append(final)
+            if(preload):
+                self.wbc = modelout # wbc = weights-biases-compute
             self.addlayer = False
             return modelout
 
-    def compute_losses(self, model_compiled):
-        m1 = model_compiled
-        num = m1[0][0][0][0]
-        print(self.model[0][1].use(0, num))
+    def preload(self, inputs, preload=True):
+        print(self.model)
+        modelout = []
+        modelout.append(inputs)
+        self.inlayer = inputs
+        for im in range(len(self.model)):  # Activation Inititalization
+            if im != 0:
+                m1 = []
+                for i in range(self.model[im][0]):
+                    m1.append(0)
+                modelout.append(m1)
+        if preload:
+            self.pm = modelout  # Preloaded Model
+        return modelout
+
+    def compute_losses(self, model_compiled, self_add=True):
+        modelout = []
+        for im in range(len(self.model)):  # Activation Inititalization
+            if im != 0:
+                m1 = []
+                for i in range(self.model[im][0]):
+                    activation = self.model[im][1]
+                    for k in range(self.model[im - 1][0]):
+                        value = self.pm[im - 1][k]
+                        weight = self.wbc[0][0][im - 1][i][k]
+                        print(f'value: {value}, weight: {weight}')
+                modelout.append(m1)
+
+
